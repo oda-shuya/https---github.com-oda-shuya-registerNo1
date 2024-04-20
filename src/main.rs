16d79f35cfac966e57fb7af1df16191fbe83bca9
Shuya <shuya.wins.forever@gmail.com>
2024-04-21 02:17:09 +0900

    会員登録画面を作成した。
    - 入力画面、確認画面、完了画面の三つ
    - main.rs内にすべてを記述

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App,web,get,post,HttpResponse,HttpServer,Responder};
+use serde::{Serialize,Deserialize};
+use tera::{Tera, Context};
+
+#[derive(Serialize,Deserialize,Clone)]
+struct FormData{
+    username : String,
+    mailaddress : String,
+    password : String
+}
+
+#[get("/")]
+async fn register(tera : web::Data<Tera>)-> impl Responder{
+    let context = Context::new();
+    tera.render("register.html",&context)
+        .map(|body| HttpResponse::Ok().content_type("text/html").body(body))
+        .unwrap_or_else(|_| HttpResponse::InternalServerError().finish())
+}
+
+#[post("/confirm")]
+async fn confirm(tera : web::Data<Tera>,form : web::Form<FormData>)-> impl Responder{
+    let mut context = Context::new();
+    context.insert("user",&form.into_inner());
+    tera.render("confirm.html",&context)
+        .map(|body| HttpResponse::Ok().content_type("text/html").body(body))
+        .unwrap_or_else(|_| HttpResponse::InternalServerError().finish())
+} 
+
+#[post("/finish")]
+async fn finish(tera : web::Data<Tera>,form : web::Form<FormData>)-> impl Responder{
+    let mut context = Context::new();
+    context.insert("user",&form.into_inner());
+    tera.render("finish.html",&context)
+        .map(|body| HttpResponse::Ok().content_type("text/html").body(body))
+        .unwrap_or_else(|_| HttpResponse::InternalServerError().finish())
+} 
+
+#[actix_web::main]
+async fn main()->std::io::Result<()>{
+    let tera = Tera::new("src/views/**").expect("Teraのインスタン生成に失敗しました");
+    HttpServer::new(move ||{
+        App::new()
+            .app_data(web::Data::new(tera.clone()))
+            .service(register)
+            .service(confirm)
+            .service(finish)   
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }

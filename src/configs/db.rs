b70c0681fd5eb395abc2c5d8697d2cf848673b22
oda-shuya <167622899+oda-shuya@users.noreply.github.com>
2024-04-28 14:26:37 +0900

    Update db.rs

--- src/configs/db.rs
+++ src/configs/db.rs
@@ -1,5 +1,5 @@
 use mysql::*;
 pub fn establish_connection()-> Pool{
-    let url = "mysql://root:Aa78611117@localhost:3306/pokupoku";
+    let url = "mysql://rootパスワード@localhost:3306/pokupoku";
     Pool::new(url).expect("接続に失敗しました")
 }
